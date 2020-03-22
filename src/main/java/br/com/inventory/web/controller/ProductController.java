bf28a63df0d62a90af86f869c3fc8dad612cb5b4
Gabriel Martins <ga.smartins94@gmail.com>
2020-03-21 23:24:26 -0300

    Corrigindo @RequestBody

--- src/main/java/br/com/inventory/web/controller/ProductController.java
+++ src/main/java/br/com/inventory/web/controller/ProductController.java
@@ -10,6 +10,7 @@ import br.com.inventory.web.mapper.ProductWebMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,7 +29,7 @@ public class ProductController {
     private final ProductWebMapper productWebMapper;
 
     @PostMapping()
-    public ResponseEntity<?> create(@Valid ProductRequestDto productRequestDto, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<?> create(@RequestBody  @Valid ProductRequestDto productRequestDto, UriComponentsBuilder uriBuilder){
         Product product = productWebMapper.mapToDomain(productRequestDto);
         Product createdProduct = createProductUseCase.create(product);
         ProductResponseDto productResponseDto = productWebMapper.mapToDto(createdProduct);
--- src/main/java/br/com/inventory/web/dto/request/ProductRequestDto.java
+++ src/main/java/br/com/inventory/web/dto/request/ProductRequestDto.java
@@ -3,12 +3,14 @@ package br.com.inventory.web.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
 
 @Getter
 @Setter
+@ToString
 public class ProductRequestDto {
 
     @JsonProperty("description")
